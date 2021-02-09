e7608cbefbd029dfa03f28fbbf0e5cc8d2867b06
halil <dospatliiski@gmail.com>
2021-02-09 11:57:04 +0200

    112

---
+++ 05_Methods/01.EmptyBelejka/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.EmptyBelejka
+{
+    class Program
+    {
+        static void PrintHeader()
+        {
+            Console.WriteLine("Header");
+        }
+
+        static void PrintMiddle()
+        {
+            Console.WriteLine("Middle");
+        }
+
+        static void PrintFooter()
+        {
+            Console.WriteLine("Footer");
+        }
+
+        static void PrintAll()
+        {
+            PrintHeader();
+            PrintMiddle();
+            PrintFooter();
+        }
+
+        static void Main(string[] args)
+        {
+            PrintAll();
+        }
+    }
+}
