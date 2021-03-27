83c7d8253427dcc0712b13ca1479597a5dab35e5
halilski1979 <halilski1979@gmail.com>
2021-03-27 14:11:21 +0200

    nnn

---
+++ 7.Rechnici/28p_SreshtaniqChislo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28p_SreshtaniqChislo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var counts = new SortedDictionary<double, int>();
+            var nums = Console.ReadLine().Split(' ').Select(double.Parse).ToList();
+            foreach (var item in nums)
+            {
+                if (counts.ContainsKey(item))
+                {
+                    counts[item]++;
+                }
+                else
+                {
+                    counts[item] = 1;
+                }
+            }
+
+            foreach (var item in counts)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+
+        }
+    }
+}
